a0d5ee9a368324d56c93ec6d9ed9d690ee4e3d1c
Anas Raza <96536838+Anas-Dew@users.noreply.github.com>
2022-01-27 20:01:40 +0530

    Little improvement in bahevior
    1. Throws error when new trying to create new account with the username which already exists in data base.
    2.Good print statement shows when you successfully create a new account.
    3. More improvements coming..

--- Public_diary.py
+++ Public_diary.py
@@ -1,11 +1,15 @@
+from pickletools import read_long1
+from cv2 import line
 from pip import main
 from termcolor import cprint
 from pyfiglet import *
 from time import sleep
+from Clear_Screen_Func import clear
 
 
 def navigate_app():
-    f = open("C:\\Users\Public\data.secret", "w+")
+
+    f = open("C:\\Users\Public\data.secret", "a")
     print("CHOOSE NUMBERS TO NEVIGATE---")
     while True:
         sleep(0.5)
@@ -23,15 +27,28 @@ def navigate_app():
 
 
 def addnotes():
+    clear()
     n_details = []
     sleep(0.6)
     while True:
         name = input("Create Username (8 Charecters): ")
         if len(name) != 8:
-            raise ValueError
+            cprint('Username isnot 8 Charecters long', 'red')
+            navigate_app()
+        with open("C:\\Users\Public\data.secret", "r") as f:
+            read = f.readlines()
+
+        for i in range(len(read)):
+            readn = read[i][9:17]
+
+            if name == readn:
+                cprint('Username Already Exists !!!', 'red')
+                sleep(0.7)
+                navigate_app()
         password = input("Create PIN (3 Digits): ")
         if len(password) != 3:
-            raise ValueError
+            cprint('PIN isnot 3 Digits long', 'red')
+            navigate_app()
         secretnote = input("MY NOTE (98 Charecters only) : ")
         n_details.append("NAME : "+name+", PASS : " +
                          password+", SECRET : "+secretnote)
@@ -40,27 +57,38 @@ def addnotes():
             f.write(str(n_details))
             f.write("\n")
             f.close()
+            cprint('\nNote successfully created.','green')
             break
 
 
+def remove(string):
+    return "".join(string.split())
+
+
 def viewnotes():
+    clear()
     e_details = []
     sleep(0.6)
     name = input("Enter Username : ")
     password = str(input("Enter PIN : "))
-    e_details.append("NAME : "+name+", PASS : "+password)
 
     with open("C:\\Users\Public\data.secret", "r") as f:
         read = f.readlines()
+
         for i in range(len(read)):
+
             readn = read[i][9:17]
             if readn == name:
                 readp = read[i][26:29]
                 if readp == password:
                     readed = read[i][40:138]
+                    clear()
+
                     cprint('\nYOUR SECRET NOTE IS : ', 'blue')
                     cprint(readed, 'green')
-                    break
+                    
+                    
+
                 else:
                     pass
             else:
@@ -72,31 +100,24 @@ def modify():  # UNDER CONSTRUCTION --------------------------------------------
     name = input("Enter Username : ")
     password = str(input("Enter PIN : "))
     e_details.append("NAME : "+name+", PASS : "+password)
+    
+    cprint('\n1 - Add Something\n0 - Exit')
+    usc = int(input())
+    if usc == 1:
+        with open("C:\\Users\Public\data.secret", "a") as mod:
+            cprint('Write here : ', 'green')
+            added_more = input()
+            joinit = remove(readed)+added_more
+            e_details.append('NAME : '+name+', PASS : ' +
+                                             password+', SECRET : '+joinit)
+            mod.writelines(str(e_details))
+            mod.write("\n")
+            mod.close()
+            
+    elif usc == 0:
+        navigate_app()
+    
 
-    with open("C:\\Users\Public\data.secret", "r") as f:
-        read = f.readlines()
-        for i in range(len(read)):
-            readn = read[i][9:17]
-            if readn == name:
-                readp = read[i][26:29]
-                if readp == password:
-                    readed = read[i][40:138]
-
-                    cprint('\nYOUR SECRET NOTE IS : ', 'blue')
-                    cprint(readed, 'green')
-                    cprint('ADD SOMETHING :', 'blue')
-
-                    add_new = input()
-                    e_details.append(
-                        "NAME : "+name+", PASS : "+password + add_new)
-                    with open("C:\\Users\Public\data.secret", "a") as a:
-                        a.write(str(e_details))
-                    break
-                else:
-                    pass
-
-            else:
-                pass
 
 
 if __name__ == "__main__":
